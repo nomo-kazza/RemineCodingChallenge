354882f5776394dc44c1d549e93a6520df165b53
jared edwards <jredwards519@gmail.com>
2017-09-26 22:35:34 -0400

    retrieving data from API and displaying in RemineTabel

--- src/CHANGEME.js
+++ src/CHANGEME.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import RemineTable from './components/Table/RemineTable/RemineTable';
+import API from './API';
 
 class Test extends Component {
-    render() {
-        return (
-            <div className="testContainer">
-                <div className="filterContainer">
-                    Your filters go here.
-                </div>
-                <RemineTable properties={[]} />
-            </div>
-        );
-    }
+  state = {
+    locations: null
+  };
+  componentWillMount() {
+    API.getLocations().then(({ data }) => {
+      console.log(data);
+      this.setState({ locations: data });
+    });
+  }
+  render() {
+    return (
+      <div className="testContainer">
+        <div className="filterContainer">Your filters go here.</div>
+        <pre>
+          <code>{JSON.stringify(this.state.locations, null, 2)}</code>
+        </pre>
+        {this.state.locations ? <RemineTable properties={this.state.locations} /> : <h1>loading</h1>}
+      </div>
+    );
+  }
 }
 
 export default Test;
